bb4e6fad98c83f59dd5f1a01126b4e1a0626457d
xiaotantanya <2634918190@qq.com>
2024-03-16 23:59:32 +0800

    config .gitignore file

--- mpi_fortran/build/external/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tanwen/study/fortran_study/mpi_fortran/external
-# Build directory: /home/tanwen/study/fortran_study/mpi_fortran/build/external
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../exprtk")
-subdirs("../fson")
-subdirs("../toml-f")
